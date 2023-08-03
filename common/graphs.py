b3c2f30c64d6aed42c73efd06bd8f0060c31815c
Gaurav Bakale <gaurav.bakale@impactanalytics.co>
2023-08-03 12:24:47 +0530

    added logic to include weights for the edges

--- common/graphs.py
+++ common/graphs.py
@@ -20,11 +20,22 @@ class Edge:
     """
         Create a edge. This is a directed edge and if undirected edge is required 2 must be manually created.
     """
-    def __init__(self, source_vertex : Vertex, destination_vertex:Vertex, undirected:bool = True):
+    def __init__(self, source_vertex : Vertex, destination_vertex:Vertex, weight:int = 1, undirected:bool = True):
         self.source_vertex = source_vertex
         self.destination_vertex = destination_vertex
         self.name = source_vertex.name + '-' + destination_vertex.name
         self.undirected = undirected
+        self.weight = weight
+
+    def get_weight(self):
+        return self.weight
+    
+    def get_source(self):
+        return self.source_vertex
+
+    def get_destination(self):
+        return self.destination_vertex
+    
     def __str__(self):
         if self.undirected:
             return f"{self.source_vertex} <--> {self.destination_vertex}"
@@ -64,11 +75,12 @@ class Graph:
             raise Exception(f"Vertex '{vertex}' not found")
         self.vertexes.pop(vertex)
 
-    def add_edge(self, source_vertex: str , destination_vertex: str):
+    def add_edge(self, source_vertex: str , destination_vertex: str, weight:int = 1):
 
         E = Edge(
             self.vertexes[source_vertex], 
             self.vertexes[destination_vertex],
+            weight,
             self.undirected
         )
         if E.name in self.all_edges.keys():
@@ -85,7 +97,8 @@ class Graph:
             self.all_edges[other_edge.name] = other_edge
     
     def list_edges(self):
-        print(f"edges: {list(self.edges.keys())}")
+        for edge_name in self.all_edges:
+            print(f"edges: {self.all_edges[edge_name]}, [{self.all_edges[edge_name].get_weight()}]")
 
     def drop_edge(self, edge_name):
         try: 
@@ -109,7 +122,7 @@ class Graph:
         return f"{self.name}"
 
 # create a graph with lists of vertexes and nodes
-def create_graph(graph_name,vertexes, edges, undirected=False):
+def create_graph(graph_name,vertexes, edges, weights = [],undirected=False):
     """
         Create a graph object.
         Usage:
@@ -119,12 +132,33 @@ def create_graph(graph_name,vertexes, edges, undirected=False):
             create_graph("G", nodes, edges, False)
     """
     G = Graph(graph_name, undirected)
-    for vertex in vertexes.split(','):
+    
+    vertexes = vertexes.split(',')
+    for vertex in vertexes:
         G.add_vertex(vertex)
-    for edge in edges.split(','):
+
+    edges = edges.split(',')
+    # if the weights are given and the count does not match of edges and weights then raise exception
+    if weights:
+        weights = [int(i) for i in weights.split(',')]
+        if len(weights) != len(edges):
+            raise Exception("the length of edges and the weights should match.")
+        
+    for i in range(len(edges)):
         try:
-            G.add_edge(edge.split('-')[0], edge.split('-')[1])
-        except ValueError as e:
+            edge = edges[i]
+            if weights:
+                weight = weights[i]
+            else:
+                weight = 1
+
+            G.add_edge(
+                edge.split('-')[0],
+                edge.split('-')[1],
+                weight
+            )
+            
+        except Exception as e:
             print(e)
     
     G.list_vertexes()
